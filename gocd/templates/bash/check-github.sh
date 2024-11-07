d56b9dcf082924b64af05aa9917bb2446863f2a4
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-11-07 00:34:41 -0800

    Remove secrets

--- gocd/templates/bash/check-github.sh
+++ gocd/templates/bash/check-github.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-/devinfra/scripts/checks/githubactions/checkruns.py \
-  --timeout-mins 60 \
-  getsentry/snuba \
-  ${GO_REVISION_SNUBA_REPO} \
-  "Tests and code coverage (test)"
+echo "Check GitHub!"
